9cfd7614c6064c4f120a6112ec7b294a67b9d240
ArghyadipBiswas <107795593+ArghyadipBiswas@users.noreply.github.com>
2023-10-04 11:00:33 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,14 +14,14 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 6235351
 API_HASH = "f68fde1378da8f85a243f2ae57f2fcf9"
-BOT_TOKEN = 5378163079:AAHhtCs9RPaEmux-UAiQOo7KVDmFRwMnd4o
+BOT_TOKEN = "5378163079:AAHhtCs9RPaEmux-UAiQOo7KVDmFRwMnd4o"
 SESSION = "BABfJNcAECJA_2mE1graeYTqIO2cQTe26JkE0VuKCGljRFV3_92UmLhdV7LbhJFBFfTvwARMS3aGzSGL6wA0XitmnnEYDVhXzF3oIzdpDcXIQmD2DfDdY2uCPO7qsKJthjkvRcPzkXwWQDDqE1S_UJtvxYApt2ypNMlFmOIGmahxqfSFU0maoaDGGgDqh6gOj1xTG2Jy_DIShdMtz4P77elfVJdgijBaLMi-HILApZ2qV-u5ctP8nmhhtOLOYE5W0aRN9cN2KTJjQJNHo0Kq471FXj8IZu4yQf--C0iCWi2MlVRAaMLBsFiYT7dG9QR8iR7NYnz-7jw8xrRTadBM_soTxFy7RgAAAAEsjtP5AA"
 FORCESUB = "nub696"
 AUTH = 5042525177
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverestricted", session_string=""BABfJNcAECJA_2mE1graeYTqIO2cQTe26JkE0VuKCGljRFV3_92UmLhdV7LbhJFBFfTvwARMS3aGzSGL6wA0XitmnnEYDVhXzF3oIzdpDcXIQmD2DfDdY2uCPO7qsKJthjkvRcPzkXwWQDDqE1S_UJtvxYApt2ypNMlFmOIGmahxqfSFU0maoaDGGgDqh6gOj1xTG2Jy_DIShdMtz4P77elfVJdgijBaLMi-HILApZ2qV-u5ctP8nmhhtOLOYE5W0aRN9cN2KTJjQJNHo0Kq471FXj8IZu4yQf--C0iCWi2MlVRAaMLBsFiYT7dG9QR8iR7NYnz-7jw8xrRTadBM_soTxFy7RgAAAAEsjtP5AA"", api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
